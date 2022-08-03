8fdd1bf64b7de5a576df40fc230ba6db97719dce
Russ Gibson <russg@rnstech.com>
2022-08-02 22:47:49 -0700

    Merged gdnative_cpp_example into parent.

--- Kakel/main.gd
+++ Kakel/main.gd
@@ -1,6 +1,5 @@
 extends Control
 
-onready var data = preload("res://bin/kakel.gdns").new()
+func _on_Sprite_position_changed(node: Node, new_pos: Vector2) -> void:
+	print("The position of " + node.name + " is now " + str(new_pos))
 
-func _on_Button_pressed():
-	$Label.text = "Data = " + data.get_data()
