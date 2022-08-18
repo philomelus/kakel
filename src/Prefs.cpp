75461705eae9944ed441a7ff210670d277d51f15
Russ Gibson <russg@rnstech.com>
2022-08-17 23:22:16 -0700

    Moved UI setup to Godot Editor.

--- src/Prefs.cpp
+++ src/Prefs.cpp
@@ -19,36 +19,6 @@ void Prefs::_bind_methods()
 Prefs::Prefs()
 {
 	FUNC_("Prefs::Prefs");
-
-	_autoLoad = memnew(CheckButton);
-	_autoLoadLabel = memnew(Label);
-	_autoSave = memnew(CheckButton);
-	_autoSaveLabel = memnew(Label);
-	_cancel = memnew(Button);
-	_centerContainer = memnew(CenterContainer);
-	_columns = memnew(SpinBox);
-	_columnsLabel = memnew(Label);
-	_gridContainer = memnew(GridContainer);
-	_hboxContainer = memnew(HBoxContainer);
-	_hiliteBlankLabel = memnew(Label);
-	_hiliteBlank = memnew(CheckButton);
-	_hiliteBlankColorLabel = memnew(Label);
-	_hiliteBlankColor = memnew(ColorPickerButton);
-	_keepAspect = memnew(CheckButton);
-	_keepAspectLabel = memnew(Label);
-	_marginContainer = memnew(MarginContainer);
-	_numbersColor = memnew(ColorPickerButton);
-	_numbersColorLabel = memnew(Label);
-	_numbersVisible = memnew(CheckButton);
-	_numbersVisibleLabel = memnew(Label);
-	_outlinesColor = memnew(ColorPickerButton);
-	_outlinesColorLabel = memnew(Label);
-	_outlinesVisible = memnew(CheckButton);
-	_outlinesVisibleLabel = memnew(Label);
-	_rows = memnew(SpinBox);
-	_rowsLabel = memnew(Label);
-	_save = memnew(Button);
-	_vboxContainer = memnew(VBoxContainer);
 }
 
 Prefs::~Prefs()
@@ -61,171 +31,55 @@ void Prefs::_ready()
 	FUNC_("Prefs::_ready");
 		
 	_globals = get_node<AppGlobals>("/root/Globals");
-	_preferences = get_node<AppPreferences>("/root/Preferences");
-	_tree = get_tree();
-
 	ERR_FAIL_COND(_globals == nullptr);
+
+	_preferences = get_node<AppPreferences>("/root/Preferences");
 	ERR_FAIL_COND(_preferences == nullptr);
 
-	Ref<Theme> theme = _globals->theme_get();
-	if (theme == nullptr)
-	{
-		theme = Ref(ResourceLoader::get_singleton()->load(_preferences->default_theme_get(), "Theme",
-														  ResourceLoader::CacheMode::CACHE_MODE_IGNORE));
-		_globals->theme_set(theme);
-	}
+	_autoLoad = get_node<CheckButton>("CenterContainer/VBoxContainer/GridContainer/AutoLoad");
+	ERR_FAIL_COND(_autoLoad == nullptr);
 
-	ERR_FAIL_COND(theme == nullptr);
+	_autoSave = get_node<CheckButton>("CenterContainer/VBoxContainer/GridContainer/AutoSave");
+	ERR_FAIL_COND(_autoSave == nullptr);
 
-	// Set out PanelContainer settings
-	set_anchors_preset(Control::LayoutPreset::PRESET_FULL_RECT);
-	set_h_size_flags(Control::SizeFlags::SIZE_EXPAND_FILL);
-	set_v_size_flags(Control::SizeFlags::SIZE_EXPAND_FILL);
-	set_h_grow_direction(Control::GrowDirection::GROW_DIRECTION_BOTH);
-	set_v_grow_direction(Control::GrowDirection::GROW_DIRECTION_BOTH);
-		
-	// Add center contqiner
-	add_child(_centerContainer);
-
-	// Add v box container
-	_vboxContainer->set_theme(theme);
-	_centerContainer->add_child(_vboxContainer);
-		
-	// Add grid container
-	_gridContainer->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER
-									 + Control::SizeFlags::SIZE_FILL);
-	_gridContainer->set_columns(2);
-	_vboxContainer->add_child(_gridContainer);
-		
-	// Add columns label
-	_columnsLabel->set_text("Columns");
-	_columnsLabel->set_tooltip("Number of columns in tile grid.");
-	_columnsLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_columnsLabel);
-
-	// Add columns
-	_columns->set_min(3);
-	_columns->set_max(99);
-	_columns->set_horizontal_alignment(HorizontalAlignment::HORIZONTAL_ALIGNMENT_CENTER);
-	_gridContainer->add_child(_columns);
-		
-	// Add rows label
-	_rowsLabel->set_text("Rows");
-	_rowsLabel->set_tooltip("Number of rows in tile grid.");
-	_rowsLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_rowsLabel);
-
-	// Add rows
-	_rows->set_min(3);
-	_rows->set_max(99);
-	_rows->set_horizontal_alignment(HorizontalAlignment::HORIZONTAL_ALIGNMENT_CENTER);
-	_gridContainer->add_child(_rows);
-
-	// Add keep aspect label
-	_keepAspectLabel->set_text("Keep Aspect Ratio");
-	_keepAspectLabel->set_tooltip("When enabled, the image aspect ratio is maintained within tiles.");
-	_keepAspectLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_keepAspectLabel);
+	_cancel = get_node<Button>("CenterContainer/VBoxContainer/MarginContainer/HBoxContainer/Cancel");
+	ERR_FAIL_COND(_cancel == nullptr);
 	
-	// Add keep aspect
-	_keepAspect->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER);
-	_gridContainer->add_child(_keepAspect);
+	_columns = get_node<SpinBox>("CenterContainer/VBoxContainer/GridContainer/Columns");
+	ERR_FAIL_COND(_columns == nullptr);
+
+	_hiliteBlank = get_node<CheckButton>("CenterContainer/VBoxContainer/GridContainer/HiliteBlank");
+	ERR_FAIL_COND(_hiliteBlank == nullptr);
+
+	_hiliteBlankColor = get_node<ColorPickerButton>("CenterContainer/VBoxContainer/GridContainer/HiliteBlankColor");
+	ERR_FAIL_COND(_hiliteBlankColor == nullptr);
+
+	_keepAspect = get_node<CheckButton>("CenterContainer/VBoxContainer/GridContainer/KeepAspect");
+	ERR_FAIL_COND(_keepAspect == nullptr);
+
+	_numbersColor = get_node<ColorPickerButton>("CenterContainer/VBoxContainer/GridContainer/NumbersColor");
+	ERR_FAIL_COND(_numbersColor == nullptr);
+
+	_numbersVisible = get_node<CheckButton>("CenterContainer/VBoxContainer/GridContainer/NumbersVisible");
+	ERR_FAIL_COND(_numbersVisible == nullptr);
+
+	_outlinesColor = get_node<ColorPickerButton>("CenterContainer/VBoxContainer/GridContainer/OutlinesColor");
+	ERR_FAIL_COND(_outlinesColor == nullptr);
+
+	_outlinesVisible = get_node<CheckButton>("CenterContainer/VBoxContainer/GridContainer/OutlinesVisible");
+	ERR_FAIL_COND(_outlinesVisible == nullptr);
+
+	_rows = get_node<SpinBox>("CenterContainer/VBoxContainer/GridContainer/Rows");
+	ERR_FAIL_COND(_autoLoad == nullptr);
+
+	_save = get_node<Button>("CenterContainer/VBoxContainer/MarginContainer/HBoxContainer/Save");
+	ERR_FAIL_COND(_save == nullptr);
 	
-	// Add outlines visible label
-	_outlinesVisibleLabel->set_text("Show Outlines");
-	_outlinesVisibleLabel->set_tooltip("When enabled, tiles are outlined.");
-	_outlinesVisibleLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_outlinesVisibleLabel);
+	_tree = get_tree();
 
-	// Add outlines visible
-	_outlinesVisible->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER);
-	_gridContainer->add_child(_outlinesVisible);
-		
-	// Add outlines color label
-	_outlinesColorLabel->set_text("Outlines Color");
-	_outlinesColorLabel->set_tooltip("Outlines are drawn in this color.");
-	_outlinesColorLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_outlinesColorLabel);
-
-	// Add outlines color
-	_gridContainer->add_child(_outlinesColor);
-
-	// Add numbers visible label
-	_numbersVisibleLabel->set_text("Show Numbers");
-	_numbersVisibleLabel->set_tooltip("When enabled, tile numbers are shown in upper left of each tile.");
-	_numbersVisibleLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_numbersVisibleLabel);
-
-	// Add numbers visible
-	_numbersVisible->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER);
-	_gridContainer->add_child(_numbersVisible);
-
-	// Add numbers color label
-	_numbersColorLabel->set_text("Numbers Color");
-	_numbersColorLabel->set_tooltip("Tile numbers are drawn in this color.");
-	_numbersColorLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_numbersColorLabel);
-		
-	// Add numbers color
-	_gridContainer->add_child(_numbersColor);
-
-	// Add hilite blank label
-	_hiliteBlankLabel->set_text("Hilite Blank Tile");
-	_hiliteBlankLabel->set_tooltip("When enabled, the blank tile is hilited.");
-	_hiliteBlankLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_hiliteBlankLabel);
-
-	// Add hilite blank
-	_hiliteBlank->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER);
-	_gridContainer->add_child(_hiliteBlank);
-
-	// Add hilite blank color label
-	_hiliteBlankColorLabel->set_text("Hilite Blank Color");
-	_hiliteBlankColorLabel->set_tooltip("When blank tile is hilited, its drawn in this color.");
-	_hiliteBlankColorLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_hiliteBlankColorLabel);
-
-    // Add hilite blank color
-	_gridContainer->add_child(_hiliteBlankColor);
-	
-	// Add auto save label
-	_autoSaveLabel->set_text("Auto Save on Exit");
-	_autoSaveLabel->set_tooltip("If \"Exit Kakel\" button is used, automatically save the current game before exit.");
-	_autoSaveLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_autoSaveLabel);
-
-	// Add auto save
-	_autoSave->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER);
-	_gridContainer->add_child(_autoSave);
-
-	// Add auto load label
-	_autoLoadLabel->set_text("Auto Load on Start");
-	_autoLoadLabel->set_tooltip("If previous automatically save game from exit\nexists, automatically load that garm on launch.");
-	_autoLoadLabel->set_mouse_filter(Control::MouseFilter::MOUSE_FILTER_STOP);
-	_gridContainer->add_child(_autoLoadLabel);
-
-	// Add auto load
-	_autoLoad->set_h_size_flags(Control::SizeFlags::SIZE_SHRINK_CENTER);
-	_gridContainer->add_child(_autoLoad);
-
-	// Add margin container
-	_vboxContainer->add_child(_marginContainer);
-
-	// Add h box container
-	_hboxContainer->set_alignment(BoxContainer::AlignmentMode::ALIGNMENT_CENTER);
-	_marginContainer->add_child(_hboxContainer);
-		
-	// Add cancel button
-	_cancel->set_text("Cancel");
-	_hboxContainer->add_child(_cancel);
-		
-	// Add save button
-	_save->set_text("Save");
-	_hboxContainer->add_child(_save);
-		
-	// Signals
-	_cancel->connect("pressed", Callable(this, "on_cancel_pressed"));
-	_save->connect("pressed", Callable(this, "on_save_pressed"));
+    // Signals
+    _cancel->connect("pressed", Callable(this, "on_cancel_pressed"));
+    _save->connect("pressed", Callable(this, "on_save_pressed"));
 
 	// Copy globals to controls
 	_autoLoad->set_pressed(_preferences->auto_load_get());
--- src/Prefs.hpp
+++ src/Prefs.hpp
@@ -1,16 +1,10 @@
 #ifndef PREFS_HPP_INCLUDED
 #define PREFS_HPP_INCLUDED
 
-#include <godot_cpp/classes/center_container.hpp>
 #include <godot_cpp/classes/check_button.hpp>
 #include <godot_cpp/classes/color_picker_button.hpp>
-#include <godot_cpp/classes/h_box_container.hpp>
-#include <godot_cpp/classes/grid_container.hpp>
-#include <godot_cpp/classes/label.hpp>
-#include <godot_cpp/classes/margin_container.hpp>
 #include <godot_cpp/classes/panel_container.hpp>
 #include <godot_cpp/classes/spin_box.hpp>
-#include <godot_cpp/classes/v_box_container.hpp>
 #include "AppGlobals.hpp"
 #include "AppPreferences.hpp"
 
@@ -21,38 +15,25 @@ namespace godot
 		GDCLASS(Prefs, PanelContainer);
 
 	private:
-		CheckButton* _autoLoad;
-		Label* _autoLoadLabel;
-		CheckButton* _autoSave;
-		Label* _autoSaveLabel;
-		Button *_cancel;
-		CenterContainer* _centerContainer;
-		SpinBox* _columns;
-		Label* _columnsLabel;
+		// Common
+		AppPreferences* _preferences;
 		AppGlobals* _globals;
-		GridContainer* _gridContainer;
-		HBoxContainer* _hboxContainer;
+
+		// UI
+		CheckButton* _autoLoad;
+		CheckButton* _autoSave;
+		Button *_cancel;
+		SpinBox* _columns;
 		CheckButton* _hiliteBlank;
 		ColorPickerButton* _hiliteBlankColor;
-		Label* _hiliteBlankColorLabel;
-		Label* _hiliteBlankLabel;
 		CheckButton* _keepAspect;
-		Label* _keepAspectLabel;
-		MarginContainer* _marginContainer;
 		ColorPickerButton* _numbersColor;
-		Label* _numbersColorLabel;
 		CheckButton* _numbersVisible;
-		Label* _numbersVisibleLabel;
 		ColorPickerButton* _outlinesColor;
-		Label* _outlinesColorLabel;
 		CheckButton* _outlinesVisible;
-		Label* _outlinesVisibleLabel;
-		AppPreferences* _preferences;
 		SpinBox* _rows;
-		Label* _rowsLabel;
 		Button* _save;
 		SceneTree* _tree;
-		VBoxContainer* _vboxContainer;
 
 	protected:
 		static void _bind_methods();
